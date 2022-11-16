d1fd1dff5bd9205a52f9cb22be69ebae0f71f83a
Nathan Kershaw <63566309+nathan-nz@users.noreply.github.com>
2022-11-17 10:28:02 +1300

    Update discourse-kanban-nav.js
    Adding changes suggested here: https://github.com/Lemon-planting-light/discourse-kanban-theme/blob/9596eb53b229efd1c2058bb590a1e96fddd67e90/javascripts/discourse-kanban/components/discourse-kanban-nav.js

--- javascripts/discourse-kanban/components/discourse-kanban-nav.js
+++ javascripts/discourse-kanban/components/discourse-kanban-nav.js
@@ -2,6 +2,7 @@ import Component from "@ember/component";
 import { inject as service } from "@ember/service";
 import discourseComputed from "discourse-common/utils/decorators";
 import DiscourseURL from "discourse/lib/url";
+import { getCurrentCategoryUrl } from "../lib/kanban-utilities";
 
 export default Component.extend({
   tagName: "a",
@@ -20,7 +21,21 @@ export default Component.extend({
   },
 
   click(event) {
+    const get_mode = () => {
+      const nowat = getCurrentCategoryUrl();
+      const mode_set = settings["default_modes"].split("|")
+      let returns = "default";
+      for (const mode_to_apply of mode_set) {
+        const sets_of_cate = mode_to_apply.split(":");
+        if (nowat === sets_of_cate[0]) {
+          returns = sets_of_cate[1];
+          break;
+        }
+      }
+      return returns;
+    }
+    
     event.preventDefault();
-    DiscourseURL.routeTo(`${this.href}?board=default`);
+    DiscourseURL.routeTo(`${this.href}?board=${get_mode()}`);
   },
 });
