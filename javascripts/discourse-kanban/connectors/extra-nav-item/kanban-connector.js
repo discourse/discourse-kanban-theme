3a86666dc7f0b788c4188afb4235afa3fbe00779
Nathan Kershaw <63566309+nathan-nz@users.noreply.github.com>
2022-11-21 11:04:39 +1300

    Update kanban-connector.js

--- javascripts/discourse-kanban/connectors/extra-nav-item/kanban-connector.js
+++ javascripts/discourse-kanban/connectors/extra-nav-item/kanban-connector.js
@@ -1,9 +1,8 @@
-import { displayConnector, getCurrentCategoryUrl } from "../../lib/kanban-utilities";
+import { displayConnector, getCurrentCategoryFromUrl } from "../../lib/kanban-utilities";
 
 export default {
   shouldRender(args, component) {
-    const slug_fixed = component.get("category.slug") || getCurrentCategoryUrl();
-    console.log(`${slug_fixed}`)
+    const slug_fixed = (args.category && args.category.slug) || getCurrentCategoryFromUrl();
     return displayConnector(slug_fixed);
   },
 };
